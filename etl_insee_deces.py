3b3cc5b96f22f5ef2b28f4752ab4aaa407ef357e
Gutkowski Florian <gutkowski@iuct-oncopole.fr>
2026-05-11 10:01:10 +0200

    modification date start date

--- etl_insee_deces.py
+++ etl_insee_deces.py
@@ -3,7 +3,7 @@ import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-from datetime import timedelta
+from datetime import datetime, timedelta
 sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -15,7 +15,7 @@ from utils.insee_loader import (
 
 default_args = {
     'owner': 'DATA-IA',
-    'start_date': days_ago(1),
+    'start_date': datetime(2026, 1, 1),  # date fixe
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
